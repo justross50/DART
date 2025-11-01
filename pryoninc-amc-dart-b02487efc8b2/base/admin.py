a7572cfd31f54933ebb2ce1eed0e22c77853adf2
Justin <93613676+justross50@users.noreply.github.com>
2025-11-01 18:58:47 -0500

    Remove Git LFS tracking and hooks

--- pryoninc-amc-dart-b02487efc8b2/base/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Event, Comment, Chat
-
-# Register your models here.
-admin.site.register(Event)
-admin.site.register(Comment)
-admin.site.register(Chat)
--- pryoninc-amc-dart-b02487efc8b2/static/base/js/functions.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    var inputVal = document.getElementById("name").value;
-    if (inputVal.trim() === "") {
-        alert("Please enter a name.");
-        return false;
-    }
-    return true;
-}
